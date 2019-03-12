d58c8db8a251a2a755e2e607ef3055249ad064ce
Medya <33479784+medyahan@users.noreply.github.com>
2019-03-12 18:58:31 +0300

    Create 12-ParaUstuVermeSorusu2.py

---
+++ 12-ParaUstuVermeSorusu2.py
@@ -0,0 +1,25 @@
+# 12.03.2019
+
+# ParaUstuVermeSorusu nun iyilestirmesi
+
+bozukPara = [1, 5, 10, 25, 50]
+
+para = input("Para Ustu: ")
+
+def paraUstu(liste, a, knownResults):
+    min = a
+    if a in liste:
+        knownResults[a] = 1
+        return 1
+    elif(knownResults[a]>0):
+        return knownResults[a]
+    else:
+        for i in [c for c in liste if c<=a]:
+            sayac = 1 + paraUstu(liste, a-i, knownResults)
+            if(sayac<min):
+                min = sayac
+                knownResults[a] = min
+    return min
+
+result = paraUstu(bozukPara, para, [0]*(para+1))
+print(result)
