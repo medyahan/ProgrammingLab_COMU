7f4524c473620294b6b18a5330c5b3239760f9b6
Medya <33479784+medyahan@users.noreply.github.com>
2019-05-27 18:57:33 +0300

    Update 26-Normalizasyon.py

--- 26-Normalizasyon.py
+++ 26-Normalizasyon.py
@@ -1,13 +1,12 @@
 import random
 
 def get_n_random_integer(n):
-    random.seed(100)
+    # random.seed(100)
     numbers = []
     for i in range(n):
         s = random.randint(-100, 100)
         numbers.append(s)
     return numbers
-
 
 def get_mean_for_n_integer(numbers):
     toplam = 0
@@ -15,14 +14,13 @@ def get_mean_for_n_integer(numbers):
     for sayi in numbers:
         toplam = toplam + sayi
         kactane = kactane + 1
-
     return toplam / kactane
-
 
 def get_std_for_n_integer(numbers):
     toplam = 0
     kactane = 0
     ortalama = get_mean_for_n_integer(numbers)
+
     for sayi in numbers:
         toplam = toplam + (sayi - ortalama) ** 2
         kactane = kactane + 1
@@ -30,7 +28,6 @@ def get_std_for_n_integer(numbers):
     var_1 = toplam / (kactane - 1)
     std_1 = var_1 ** 0.5
     return std_1
-
 
 def normalizasyon(numbers):
     new_numbers = []
@@ -43,23 +40,7 @@ def normalizasyon(numbers):
 
     return new_numbers
 
-
-def get_mean_one_std_neigbor_ratio(numbers):
-    kactane = 0
-    toplamKacSayi = 0
-    ortalama = get_mean_for_n_integer(numbers)
-    standartd_sapma = get_std_for_n_integer(numbers)
-
-    low = ortalama - standartd_sapma
-    high = ortalama + standart_sapma
-    for x in numbers:
-        toplamKacSayi = toplamKacSayi + 1
-        if (x > low and x < high):
-            kactane = kactane + 1
-    return kactane / toplamKacSayi
-
-
-def get_mean_swap_in_insertion(numTrials, numInt):
+def get_mean_of_swap_in_insertion(numTrials, numInt):
     swap_sayilari = []
     for i in range(numTrials):
         sayilar_1 = get_n_random_integer(numInt)
@@ -72,21 +53,7 @@ def get_mean_swap_in_insertion(numTrials, numInt):
 
     return numInt, int(mean_1), int(std_1)
 
-
-def bubblesort(arr):
-    n = len(arr)
-    karsilastirma_sayisi = 0
-    yerdegistirme_sayisi = 0
-    for i in range(n):
-        karsilastirma_sayisi += 1
-        for j in range(0, n - i - 1):
-            if (arr[j] > arr[j + 1]):
-                yerdegistirme_sayisi += 1
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return karsilastirma_sayisi, yerdegistirme_sayisi
-
-
-def get_mean_swap_in_bubble(numTrials, numInt):
+def get_mean_of_swap_in_bubble(numTrials, numInt):
     swap_sayilari = []
     for i in range(numTrials):
         sayilar_1 = get_n_random_integer(numInt)
@@ -98,28 +65,62 @@ def get_mean_swap_in_bubble(numTrials, numInt):
 
     return numInt, int(mean_1), int(std_1)
 
-
 def insertion(numbers):
     sayilar_2 = numbers
+    temp = 0
     karsilastirma_sayisi = 0
     yerdegistirme_sayisi = 0
     length_1 = len(sayilar_2)
-    print(sayilar_2)
+    #print(sayilar_2)
+
     for i in range(1, length_1):
         for j in range(i, 0, -1):
             karsilastirma_sayisi += 1
+
             if (sayilar_2[j] < sayilar_2[j - 1]):
                 yerdegistirme_sayisi += 1
                 sayilar_2[j - 1] = sayilar_2[j]
                 sayilar_2[j] = temp
-
-        for x in numbers:
-            toplamKacSayi = toplamKacSayi + 1
-            if (x > low and x < high):
-                kactane = kactane + 1
+            else:
+                break
+        #print(sayilar_2)
 
     return sayilar_2, karsilastirma_sayisi, yerdegistirme_sayisi
 
+def bubblesort(arr):
+    n = len(arr)
+    karsilastirma_sayisi = 0
+    yerdegistirme_sayisi = 0
 
-sayilar_1 = get_n_random_integer(10)
-sayilar_2 = insertion(sayilar_1)
+    for i in range(n):
+        karsilastirma_sayisi += 1
+        for j in range(0, n - i - 1):
+            if (arr[j] > arr[j + 1]):
+                yerdegistirme_sayisi += 1
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return karsilastirma_sayisi, yerdegistirme_sayisi
+
+def get_mean_one_std_neigbor_ratio(numbers):
+    kactane = 0
+    toplamKacSayi = 0
+    ortalama = get_mean_for_n_integer(numbers)
+    standartd_sapma = get_std_for_n_integer(numbers)
+
+    low = ortalama - standartd_sapma
+    high = ortalama + standart_sapma
+
+    for x in numbers:
+        toplamKacSayi = toplamKacSayi + 1
+        if (x > low and x < high):
+            kactane = kactane + 1
+    return kactane / toplamKacSayi
+
+sayilar = get_n_random_integer(5)
+ortalama = get_mean_for_n_integer(sayilar)
+standartsapma = get_std_for_n_integer(sayilar)
+yenisayilar = normalizasyon(sayilar)
+
+print("Sayilar: ", sayilar)
+print("Ortalama: ", ortalama)
+print("Standart Sapma: ", standartsapma)
+print("Normalize Edilmis Yeni Sayilar: ", yenisayilar)
