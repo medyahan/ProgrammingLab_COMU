6cdd14ed3847e8e6d67541180e26102c080faeec
Medya <33479784+medyahan@users.noreply.github.com>
2019-05-27 23:08:59 +0300

    Update 28-ShellSort.py

--- 28-ShellSort.py
+++ 28-ShellSort.py
@@ -1,33 +1,39 @@
+# 07.05.2019
+
+# Shell sort ile diziyi sıralama ve karşılaştırma-yer değiştirme sayılarını bulma
+
 import random
 
-def get_n_random_integer(n):
-    #random.seed(10) # ayni random degerlere ulasmak icin
-    numbers = []
-    for i in range(n):
-        s = random.randint(-10,10)
-        numbers.append(s)
-    return numbers
+def shellSort(arr):
+    n = len(arr)
+    gap = int(n / 2)
 
-def ShellSort(arr):
-    n=len(arr)
-    gap=n//2
-    karsilastirma_sayisi=0
-    yerdegistirme_sayisi=0
-    while gap > 0 :
-        for i in range(gap,n):
-            karsilastirma_sayisi+=1
-            temp=arr[i]
-            j=i
+    karsilastirmaSayisi = 0
+    yerDegistirmeSayisi = 0
 
-            while j >= gap and arr[j-gap]>temp:
-                karsilastirma_sayisi+=1
-                yerdegistirme_sayisi+=1
+    while(gap >  0):
+        for i in range(gap, n):
+            karsilastirmaSayisi += 1
+            temp = arr[i]
+            j = i
+            while(j >= gap  and arr[j-gap] > temp):
+                karsilastirmaSayisi += 1
+                yerDegistirmeSayisi += 1
                 arr[j] = arr[j-gap]
                 j -= gap
             arr[j] = temp
-        gap//=2
-    return karsilastirma_sayisi,yerdegistirme_sayisi
+        gap //= 2
+    return arr, karsilastirmaSayisi, yerDegistirmeSayisi
 
-random.seed(42)
-arr=get_n_random_integer(100)
-print(ShellSort(arr))
+def createRandomArray(n):
+    arr = []
+    for i in range(n):
+        x = random.randint(-n, n)
+        arr.append(x)
+    return arr
+
+
+arr = createRandomArray(100)
+
+print(shellSort(arr))
+
